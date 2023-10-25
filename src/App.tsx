b4f03a845dcba4d3d0486ee8451ffa2bda4aabb9
Leandro Diaz <leandritodiaz00@gmail.com>
2023-10-25 14:37:38 -0300

    feat: :sparkles: added table with dollar conversion values

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Home } from "./components/Home";
 function App() {
   return (
     <main className="flex h-screen items-center justify-center bg-blue-200">
-      <div className="m-auto h-full max-h-96 max-w-screen-md flex-1">
+      <div className="m-auto h-full max-h-96 max-w-screen-lg flex-1 px-4">
         <div className="h-full w-full rounded-3xl bg-white p-8 shadow-lg">
           <Home />
         </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,27 @@
+import cotizaciones from "../cotizaciones.json";
+
 export const Home = () => {
   return (
     <>
       <main className="flex h-full gap-4">
-        <section className="flex-1">Left</section>
-        <section className="flex-1 rounded-2xl bg-blue-400 p-8 text-white">
-          Right
+        <section className="flex-1"></section>
+        <section className="flex-1 overflow-y-auto rounded-2xl bg-blue-400 p-8 text-white">
+          <ul className="flex flex-col gap-4">
+            {cotizaciones.map((D) => (
+              <li
+                key={D.nombre}
+                className="flex items-center justify-between gap-4"
+              >
+                <div className="text-base text-blue-100">{D.nombre}</div>
+                <div className="text-3xl font-bold text-blue-600">
+                  {D.venta.toLocaleString("es-AR", {
+                    style: "currency",
+                    currency: "ARS",
+                  })}
+                </div>
+              </li>
+            ))}
+          </ul>
         </section>
       </main>
     </>

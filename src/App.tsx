e61bb32c88b958b431fe7031eafb78a6a5d3314f
Leandro Diaz <leandritodiaz00@gmail.com>
2023-10-25 13:13:32 -0300

    feat: :tada: First Commit
    add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="mx-auto text-center text-3xl font-bold text-pink-500">
+          Testing Tailwind
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
